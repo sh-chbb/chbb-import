4f2201d8c8ab8b7ad5ec869d11ff7e57c28eb6ae
James Gu <james_gu14@hotmail.com>
2018-04-01 20:41:01 +0800

    update import celebrities

--- importCelebrities.js
+++ importCelebrities.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const _ = require('lodash')
 const path = require('path')
 const fs = require('fs')
 const CsvReader = require('csv-reader')
@@ -7,111 +8,132 @@ const Promise = require('bluebird')
 const mysql = require('promise-mysql')
 const pinyin = require('pinyin')
 
+const AipFaceClient = require('baidu-aip-sdk').face
+const config = require('config')
+const baiduConfig = config.get('baiduConfig')
+const APP_NAME = baiduConfig.APP_NAME
+const APP_KEY = baiduConfig.APP_KEY
+const APP_SECRET = baiduConfig.APP_SECRET
+const GROUP_ID = baiduConfig.GROUP_ID
+
 const celebrityFile = path.join(__dirname, '/content/celebrity/celebrity.csv')
 const imgPath = path.join(__dirname, '/content/celebrity/img/')
 
 const inputStream = fs.createReadStream(celebrityFile, 'utf8')
 
-let maleList = []
-let femaleList = []
+let images = []
+let celebrityList = []
 
 const loopImages = () => {
 
-  return fs.readdirSync(imgPath)
+  images = fs.readdirSync(imgPath)
 }
 
+const readCsv = () => {
 
+  return new Promise(resolve => {
 
-const processImport = (images) => {
+    let celebrities = []
 
-  Promise.resolve().then(() => {
-
-    return new Promise(resolve => {
-
-      let celebrities = []
-
-      inputStream
-        .pipe(CsvReader({
-          parseNumbers: true,
-          parseBooleans: true,
-          trim: true
-        }))
-        .on('data', function (row) {
-          celebrities.push(row)
-        })
-        .on('end', function () {
-          resolve(celebrities)
-        })
-    }).then((celebrities) => {
-
-      Promise.each(celebrities, celebrity => {
-
-        return new Promise(resolve => {
-
-          let name = celebrity[1]
-          let gender = celebrity[2]
-          let dob = celebrity[3]
-          let job = celebrity[4]
-          let nationality = celebrity[5]
-          let birthCity = celebrity[6]
-          let desc = celebrity[7]
-          let url = celebrity[8]
-
-          let celeb = {
-            name: name,
-            gender: gender,
-            dob: dob,
-            job: job,
-            nationality: nationality,
-            birthCity: birthCity,
-            desc: desc,
-            url: url
-          }
-
-          // console.log(`Name: ${name}, gender: ${gender}, url: ${url}`)
-          if (gender == '男') {
-            
-            maleList.push(celeb)
-          } else if (gender == '女') {
-
-            femaleList.push(celeb)
-          } else {
-
-            console.log(celebrity[0], 'wrong gender')
-          }
-          resolve()
-        })
-      }).then(() => {
-
-        // console.log(`Male: ${maleList.length}`)
-        // console.log(`Female: ${femaleList.length}`)
-        let wholeList = maleList.concat(femaleList)
-
-        Promise.each(wholeList, person => {
-
-          return new Promise(resolve => {
-
-            let pinyinName = pinyin(person.name, {
-              style: pinyin.STYLE_NORMAL, 
-              heteronym: false
-            })
-            
-            let res = findImage(images, pinyinName.join(''))
-
-            if (res.length < 1) {
-              console.log(person.name)
-            }
-            // console.log(`Person: ${person.name}, Image: ${fs.existsSync(personImg)}; Young Image: ${fs.existsSync(youthImg)}`)
-            resolve()
-          })
-        }).catch((err) => {
-          
-          console.log(err)
-        })
+    inputStream
+      .pipe(CsvReader({
+        parseNumbers: true,
+        parseBooleans: true,
+        trim: true
+      }))
+      .on('data', function (row) {
+        celebrities.push(row)
       })
+      .on('end', function () {
+        resolve(celebrities)
+      })
+  })
+}
+
+const parseCelebrity = (celebrity) => {
+
+  return new Promise(resolve => {
+
+    let pinyinName = pinyin(name, {
+      style: pinyin.STYLE_NORMAL, 
+      heteronym: false
+    })
+
+    let name = celebrity[1]
+    let gender = celebrity[2]
+    let dob = celebrity[3]
+    let job = celebrity[4]
+    let nationality = celebrity[5]
+    let birthCity = celebrity[6]
+    let desc = celebrity[7]
+    let url = celebrity[8]
+
+    let celeb = {
+      name: name,
+      pinyin: pinyinName,
+      gender: gender,
+      dob: dob,
+      job: job,
+      nationality: nationality,
+      birthCity: birthCity,
+      desc: desc,
+      url: url,
+      images: []
+    }
+
+    let res = findImage(images, pinyinName.join(''))
+
+    res.forEach(r => {
+      
+      celeb.images.push(`${imgPath}${r}`)
+    })
+
+    // console.log(`Name: ${name}, gender: ${gender}, url: ${url}`)
+    if (gender == '男' || gender == '女') {
+      
+      celebrityList.push(celeb)
+    } else {
+
+      console.log(celebrity[0], 'wrong gender')
+    }
+    resolve()
+  })
+}
+
+const output = () => {
+
+  Promise.each(celebrityList, celebrity => {
+
+    Promise.each(celebrity.images, img => {
+
+      updateUser(`${celebrity.pinyin}_${_.random(10)}`, celebrity.name, img)
     })
   })
 }
+
+function updateUser(uid, userInfo, imgPath) {
+
+  return new Promise(function(resolve, reject) {
+
+    let faceClient = new AipFaceClient(APP_NAME, APP_KEY, APP_SECRET)
+    let stream = base64Encode(imgPath)
+    
+    var options = {
+      'action_type': 'replace'
+    }
+
+    faceClient.updateUser(uid, userInfo, GROUP_ID, stream, options).then(function(result) {
+      
+      console.log(`Face saves succeed - ${JSON.stringify(result)}`)
+      resolve(result)
+    }).catch(function(err) {
+      // 如果发生网络错误
+      console.log(err)
+      reject(err)
+    })
+  })
+}
+
 
 function findImage(images, fileName) {
 
@@ -126,7 +148,16 @@ function findImage(images, fileName) {
   return result
 }
 
+function base64Encode(file) {
+  // read binary data
+  var bitmap = fs.readFileSync(file)
+  // convert binary data to base64 encoded string
+  return new Buffer(bitmap).toString('base64')
+}
+
 Promise.resolve()
   .then(() => loopImages())
-  .then((images) => processImport(images))
+  .then(() => readCsv())
+  .each((celebrity) => parseCelebrity(celebrity))
+  .then(() => output())
 
